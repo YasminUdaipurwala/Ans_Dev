566e666b6069b74e08c2e818e1d76307ba804bb7
YasminUdaipurwala <41758568+YasminUdaipurwala@users.noreply.github.com>
2018-07-27 10:52:06 -0500

    Updated MyFirstRunbook-Workflow.ps1 in Azure Automation.

--- Runbooks/Ans_Dev/MyFirstRunbook-Workflow.ps1
+++ Runbooks/Ans_Dev/MyFirstRunbook-Workflow.ps1
@@ -1,4 +1,4 @@
 workflow MyFirstRunbook-Workflow
 {
-Write-Output "Hello World.This is my first edit to the RunBook"
+Write-Output "Hello World.This is my first edit to the RunBook.This is my second edit."
 }
